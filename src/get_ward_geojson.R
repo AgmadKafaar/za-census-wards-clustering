9aab8bf76e3346d6eb5c732ea2ce42d41c78c03a
Patrick Dinneen <patrick.dinneen@gmail.com>
2017-02-14 07:39:30 +0200

    Get geojson from mapit for all listed wards

---
+++ src/get_ward_geojson.R
@@ -0,0 +1,23 @@
+require(stringr)
+require(httr)
+
+chunk <- function(x, chunkSize) {
+  split(x, cut(seq_along(x), 
+               seq(from = 0, to = length(x) + chunkSize, by = chunkSize), 
+               labels=FALSE))
+}
+
+# Annoyingly mapit doesn't resolve geojson for all the ward codes in the census, so we need to extract the available ward codes
+wardSummaries <- GET("https://mapit.code4sa.org/areas/WD") %>% content(as = "text")
+wardCodes <- str_match_all(pattern = '"MDB"\\: "(.*?)"', wardSummaries)[[1]][,2]
+
+# GET geojson 300 wards at time so as not to produce URLs that are too long
+wardGeojsonRequests <- lapply(chunk(wardCodes, 300), function(wardCodes) {
+  joinedWardCodes <- paste(sprintf("MDB:%s", wardCodes), collapse = ",")
+  GET(sprintf("https://mapit.code4sa.org/areas/%s.geojson", joinedWardCodes))
+})
+
+for (i in seq_along(wardGeojsonRequests)) {
+  request <- wardGeojsonRequests[[i]]
+  write(content(request, as = "text"), file = sprintf("data/wards-%s.geojson", i))
+}
